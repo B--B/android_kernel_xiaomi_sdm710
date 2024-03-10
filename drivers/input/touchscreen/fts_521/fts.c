0c921e21aa3eb84b3dfbda8192b18c86733cbc19
Marco Zanin <mrczn.bb@gmail.com>
2024-03-10 02:20:48 +0100

    treewide: adapt code for new perf critical API
    Bring it inline with most recent changes made by Sultan
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- drivers/input/touchscreen/fts_521/fts.c
+++ drivers/input/touchscreen/fts_521/fts.c
@@ -3344,7 +3344,7 @@ static int fts_interrupt_install(struct fts_ts_info *info)
 
 	/* disable interrupts in any case */
 	error = fts_disableInterrupt();
-	info->board->irq_flags |= IRQF_PERF_CRITICAL;
+	info->board->irq_flags |= IRQF_PERF_AFFINE;
 	logError(1, "%s Interrupt Mode\n", tag);
 	if (request_threaded_irq(info->client->irq, NULL, fts_event_handler, info->board->irq_flags,
 			 FTS_TS_DRV_NAME, info)) {
